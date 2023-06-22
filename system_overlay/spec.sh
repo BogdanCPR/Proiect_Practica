97e74985e74607c278339c37a9c06426ca8f076b
Bogdan Caprita <107889800+BogdanCPR@users.noreply.github.com>
2023-06-22 13:29:38 +0300

    script specificatii sistem

---
+++ system_overlay/spec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Specificatii sistem:"
+modelProcesor=`lscpu | grep "Model name" | sed s/"Model name: "//`
+modelProcesor="Model CPU: $modelProcesor"
+
+memorieRAM=`free -h | tr -s " " | cut -f2 -d" " | tail -2 | head -1`
+memorieRAM="Memorie RAM: $memorieRAM"
+
+disk=`df -h | grep sda3 | tr -s " " | cut -f2 -d" "`
+disk="Stocare: $disk"
+
+SO=`lsb_release -a | grep Description | tr -s " \t" | sed "s/Description:\t//"`
+SO="Versiune SO: $SO"
+echo $modelProcesor
+echo $memorieRAM
+echo $disk
+echo $SO
