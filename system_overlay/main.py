4e182c37c6d542fc6d8374d2d07d3653c53c56e9
Bogdan Caprita <107889800+BogdanCPR@users.noreply.github.com>
2023-06-29 09:27:00 +0300

    FINAL PRODUCT

--- system_overlay/main.py
+++ system_overlay/main.py
@@ -9,7 +9,7 @@ os.environ['TERM'] = 'xterm'
 root = tk.Tk()
 root.overrideredirect(True)
 root.configure(background="#656565")
-root.geometry('{}x{}+{}+{}'.format(380, 310, root.winfo_screenwidth() - 400, 50))
+root.geometry('{}x{}+{}+{}'.format(350, 350, root.winfo_screenwidth() - 400, 50))
 root.attributes('-topmost', True)
 
 output_spec = subprocess.check_output(['./spec.sh'])
@@ -143,30 +143,104 @@ def tools_page():
     script_path = "/home/student/PycharmProjects/ToolsWindow/main.py"
     subprocess.Popen(['python', script_path])
 
+
+def install_tool():
+    install_window = tk.Toplevel(root)
+    install_window.title("Install app")
+    install_window.geometry("400x250+900+100")
+    install_window.configure(background="#656565")
+
+    install_label = tk.Label(install_window, text="App Manager Tool", font=('Arial', 20), fg='#C0FF6B',
+                             background="#656565")
+    install_label.pack()
+
+    app_entry = tk.Entry(install_window, width=30, show="")
+    app_entry.insert(0, "Application Name...")
+    app_entry.pack(pady=10)
+
+    pass_entry = tk.Entry(install_window, width=30, show="")
+    pass_entry.insert(0, "User Password...")
+    pass_entry.pack(pady=10)
+
+    def install_app():
+        app_name = app_entry.get()
+        password = pass_entry.get()
+        command = ["sudo", "-S", "apt-get", "install", app_name]
+        process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                   universal_newlines=True)
+        stdout, stderr = process.communicate(input=password + "\n")
+
+        if process.returncode != 0:
+            print("Comanda a eșuat. Mesajul de eroare este:")
+            print(stderr)
+        else:
+            print("Comanda a fost executată cu succes. Iată ieșirea:")
+            print(stdout)
+
+    def remove_app():
+        app_name = app_entry.get()
+        password = pass_entry.get()
+        command = ["sudo", "-S", "apt-get", "remove", app_name]
+        process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                   universal_newlines=True)
+        stdout, stderr = process.communicate(input=password + "\n")
+
+        if process.returncode != 0:
+            print("Comanda a eșuat. Mesajul de eroare este:")
+            print(stderr)
+        else:
+            print("Comanda a fost executată cu succes. Iată ieșirea:")
+            print(stdout)
+
+    install_frame = tk.Frame(install_window)
+    install_frame.pack(side='bottom', pady=15)
+
+    button_install = tk.Button(install_frame, text="Install", command=install_app, relief="groove", highlightbackground="#C0FF6B")
+    button_install.pack(side='left')
+    button_remove = tk.Button(install_frame, text="Remove", command=install_app, relief="groove",
+                       highlightbackground="#C0FF6B")
+    button_remove.pack(side='left')
+    install_window.mainloop()
+
 thread1 = threading.Thread(target=update_label_sys)
 thread2 = threading.Thread(target=update_label_net)
 thread3 = threading.Thread(target=update_label_time)
 
-button_graph = tk.Button(root, text="Graph", command=start_grafic, relief="groove", highlightbackground="#C0FF6B")
+
+frame2 = tk.Frame(root)
+frame2.pack(side='bottom')
+
+frame1 = tk.Frame(root)
+frame1.pack(side='bottom')
+
+
+
+button_graph = tk.Button(frame2, text="Graph", command=start_grafic, relief="groove", highlightbackground="#C0FF6B")
 button_graph.configure(background="#D5D5D5")
 button_graph.pack(side='left')
 
 
-button_hardware = tk.Button(root, text="Hardware", command=show_hardware, relief="groove", highlightbackground="#C0FF6B")
+button_hardware = tk.Button(frame1, text="Hardware", command=show_hardware, relief="groove", highlightbackground="#C0FF6B")
 button_hardware.configure(background="#D5D5D5")
 button_hardware.pack(side='left')
 
-button_services = tk.Button(root, text="Services", command=show_services, relief="groove", highlightbackground="#C0FF6B")
+button_services = tk.Button(frame1, text="Services", command=show_services, relief="groove", highlightbackground="#C0FF6B")
 button_services.configure(background="#D5D5D5")
 button_services.pack(side='left')
 
-button_processes = tk.Button(root, text="Processes", command=tools_page, relief="groove", highlightbackground="#C0FF6B")
+button_processes = tk.Button(frame2, text="Processes", command=tools_page, relief="groove", highlightbackground="#C0FF6B")
 button_processes.configure(background="#D5D5D5")
 button_processes.pack(side='left')
 
-button_quit = tk.Button(root, text="Quit", command=quit_app, relief="groove", highlightbackground="#C0FF6B")
+button_install = tk.Button(frame2, text="Install", command=install_tool, relief="groove", highlightbackground="#C0FF6B")
+button_install.configure(background="#D5D5D5")
+button_install.pack(side='left')
+
+button_quit = tk.Button(frame2, text="Quit", command=quit_app, relief="groove", highlightbackground="#C0FF6B")
 button_quit.configure(background="#D5D5D5")
 button_quit.pack(side='left')
+
+
 
 thread1.start()
 thread2.start()
